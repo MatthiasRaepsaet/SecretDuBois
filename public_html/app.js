1720aecd6e6a1e9a95e52c23d278867edbdcbc67
Matthias Raepsaet <Matthias Raepsaet>
2016-10-20 19:55:35 +0200

    welkompagina af + img carousel homepage + fix kamers (achtergrond needs fix)

--- public_html/app.js
+++ public_html/app.js
@@ -5,19 +5,20 @@
  */
 var app = angular.module("secret", []);
 app.factory('kamers', [function () {
+       
         var k = {
             kamers: [
-                {title: 'Bievre', kleur: ' green darken-4', highkleur: " green lighten-4", imgurl: "../images/bever.jpg", huidig: "Ze laten zich zelden zien maar ze zijn er wel, getuige de dammen die ze bouwen. Sinds een 15-tal jaar leven er opnieuw bevers in de Naamse Ardennen .Ooit waren ze hier massaal aanwezig maar zo’n honderd jaar geleden werd de laatste opgegeten of geslacht voor zijn pels. Na een herintroductie in Frankrijk zijn de dieren uitgezworven via het water en naar schatting leven er in de Naamse Ardennen momenteel een dertig tot veertig koppels. “De naam van het dorp Bièvre komt trouwens van ‘bever’  ", buttonState: "Toon Meer",
+                {title: 'Bievre', kleur: ' lime darken-3', highkleur: " lime lighten-2", imgurl: "../images/bever.jpg", huidig: "Ze laten zich zelden zien maar ze zijn er wel, getuige de dammen die ze bouwen. Sinds een 15-tal jaar leven er opnieuw bevers in de Naamse Ardennen .Ooit waren ze hier massaal aanwezig maar zo’n honderd jaar geleden werd de laatste opgegeten of geslacht voor zijn pels. Na een herintroductie in Frankrijk zijn de dieren uitgezworven via het water en naar schatting leven er in de Naamse Ardennen momenteel een dertig tot veertig koppels. “De naam van het dorp Bièvre komt trouwens van ‘bever’  ", buttonState: "Toon Meer",
                     short: 'Ze laten zich zelden zien maar ze zijn er wel, getuige de dammen die ze bouwen. Sinds een 15-tal jaar leven er opnieuw bevers in de Naamse Ardennen .Ooit waren ze hier massaal aanwezig maar zo’n honderd jaar geleden werd de laatste opgegeten of geslacht voor zijn pels. Na een herintroductie in Frankrijk zijn de dieren uitgezworven via het water en naar schatting leven er in de Naamse Ardennen momenteel een dertig tot veertig koppels. “De naam van het dorp Bièvre komt trouwens van ‘bever’  ',
                     long: 'Ze laten zich zelden zien maar ze zijn er wel, getuige de dammen die ze bouwen. Sinds een 15-tal jaar leven er opnieuw bevers in de Naamse Ardennen .Ooit waren ze hier massaal aanwezig maar zo’n honderd jaar geleden werd de laatste opgegeten of geslacht voor zijn pels. Na een herintroductie in Frankrijk zijn de dieren uitgezworven via het water en naar schatting leven er in de Naamse Ardennen momenteel een dertig tot veertig koppels. “De naam van het dorp Bièvre komt trouwens van ‘bever’.  Wonen doen bevers met zijn allen in een burcht, een stapel hout die klaar lijkt te liggen voor een reusachtig kampvuur. Maar waarom stoppen bevers zoveel energie in het opzetten van dammen? Om makkelijk zijn voedsel (planten, schors en takken van bomen) te transporteren, heeft de bever (diep) water nodig. Hij is heel behendig in het water.\n\
                            Zo behoren de dammen in de Ardennen tot de spectaculairste van Europa. Het lijkt hen goed te bevallen in ons landje.  De gekende dam van de bevers vinden we wel maar  een bever laat zich niet gauw zien, doch wordt het bewijs van zijn aanwezigheid bevestigd door het knaagwerk . Je kan er hun bouwwerken met eigen ogen bewonderen tijdens een speciaal uitgestippelde beverwandeling. Het beste moment om bevers te observeren is de aanloop naar de winter, dan zijn ze volop bezig hun enorme houtvoorraden aan te leggen.'},
-                {title: 'Yvette', kleur: ' purple darken-2', highkleur: " purple", imgurl: "", huidig: "", buttonState: "Toon Meer",
+                {title: 'Yvette', kleur: ' lime darken-3', highkleur: " lime lighten-2", imgurl: "", huidig: "", buttonState: "Toon Meer",
                     short: 'kort',
                     long: 'lang'},
-                {title: 'Saarloos', kleur: ' red lighten-1', highkleur: " red", imgurl: "", huidig: "", buttonState: "Toon Meer",
+                {title: 'Saarloos', kleur: ' lime darken-3', highkleur: " lime lighten-2", imgurl: "", huidig: "", buttonState: "Toon Meer",
                     short: 'kort',
                     long: 'lang'},
-                {title: 'La Tour', kleur: ' red darken-4', highkleur: " red darken", imgurl: "", huidig: "", buttonState: "Toon Meer",
+                {title: 'La Tour', kleur: ' lime darken-3', highkleur: " lime lighten-2", imgurl: "", huidig: "", buttonState: "Toon Meer",
                     short: 'kort',
                     long: 'lang'}]
         };
